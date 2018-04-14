3243f07f7dbfa57e657b1e6f2971385f9f13106c
1amBulletproof <brandon.tarney@gmail.com>
2018-04-14 15:44:44 -0400

    +angular framework, +css styling, +embed img, +html content

--- birthday_gift_29/js/app.js
+++ birthday_gift_29/js/app.js
@@ -1,57 +1,21 @@
-(function () {
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('App', [])
-.controller('AppController', AppController);
+  angular.module('App', [])
+    .controller('AppController', AppController);
 
-AppController.$inject = ['$scope'];
-function AppController($scope) {
-  $scope.userInput = "list comma separated dishes you usually have for lunch*";
-  $scope.messageToUser = "";
+  AppController.$inject = ['$scope', '$timeout'];
 
-  $scope.checkUserInput = function () {
-    $scope.messageToUser = "Checking User Input....";
-    var numberOfEntries = calculateNumberOfEntries($scope.userInput)
-    if (numberOfEntries > 0 && numberOfEntries <= 3) {
-      $scope.myStyle = getMsgCss("green")
-      $scope.messageToUser = "Enjoy!"
-    } else if (numberOfEntries > 3) {
-      $scope.myStyle = getMsgCss("green")
-      $scope.messageToUser = "Too much!"
-    } else if (numberOfEntries == 0) {
-      $scope.myStyle = getMsgCss("red")
-      $scope.messageToUser = "Please enter data first"
-    } else {
-      $scope.myStyle = getMsgCss("red")
-      $scope.messageToUser = "Whoops! There was an error"
+  function AppController($scope, $timeout ) {
+
+    $scope.hideStatus = true;
+
+    $scope.showHiddenContent = function() {
+      console.log("show meme");
+      $scope.hideStatus = false
+      $timeout(function () { console.log("showing description/details");}, 5000);
     }
 
   };
-
-  function calculateNumberOfEntries(string) {
-    // console.log(string)
-    if (string == "") {
-      return 0
-    }
-    var splitString = string.split(',')
-    // console.log(splitString)
-
-    //BONUS
-    var cleanArray = splitString.filter(function(n){ return n.trim() != ""})
-    // console.log(cleanArray)
-
-    return cleanArray.length
-  }
-
-  function getMsgCss(color) {
-    var style = {}
-    style['border'] = "solid"
-    style['border-color'] = color
-    style['color'] = color
-    // console.log(style)
-    return style
-  }
-
-}
 
 })();
